faf55a516a577f860565254f52e1bcb790503a6d
司华山 <sihuashanxq@outlook.com>
2017-09-17 21:46:11 +0800

    绑定

--- Zarf/Mapping/Bindings/Binders/EntityMemberBinder.cs
+++ Zarf/Mapping/Bindings/Binders/EntityMemberBinder.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Linq.Expressions;
+using Zarf.Extensions;
+using System.Linq;
 
 namespace Zarf.Mapping.Bindings
 {
@@ -7,7 +9,27 @@ namespace Zarf.Mapping.Bindings
     {
         public Expression Bind(IBindingContext bindingContext)
         {
-            throw new NotImplementedException();
+            var memberInfoType = bindingContext.Member.GetMemberInfoType();
+            var memberAccess = Expression.MakeMemberAccess(bindingContext.EntityObject, bindingContext.Member);
+
+            if (!ReflectionUtil.SimpleTypes.Contains(memberInfoType))
+            {
+                var typeBindingContext = new BindingContext(memberInfoType, bindingContext.EntityObject);
+                var binder = EntityBinderProvider.Default.GetBinder(typeBindingContext);
+
+                if (binder != null)
+                {
+                    var binding = binder.Bind(typeBindingContext);
+                    return Expression.Assign(memberAccess, binding);
+                }
+            }
+            else
+            {
+                var memAccess = Expression.MakeMemberAccess(bindingContext.EntityObject, bindingContext.Member);
+                return Expression.Assign(memAccess, null);
+            }
+
+            return null;
         }
     }
 }
