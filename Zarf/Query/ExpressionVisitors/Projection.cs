61014eae383b966ec7cd9b02d797b16f379f356b
司华山 <sihuashanxq@outlook.com>
2017-09-21 23:23:46 +0800

    生成SQL列顺序

---
+++ Zarf/Query/ExpressionVisitors/Projection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+
+namespace Zarf.Query.ExpressionVisitors
+{
+    public class Projection
+    {
+        public Expression Expression { get; set; }
+
+        public int Ordinal { get; set; }
+
+        public MemberInfo Member { get; set; }
+    }
+}
--- Zarf/Query/ExpressionVisitors/ProjectionExpressionVisitor.cs
+++ Zarf/Query/ExpressionVisitors/ProjectionExpressionVisitor.cs
@@ -4,55 +4,86 @@ using System.Linq.Expressions;
 using System.Linq;
 using Zarf.Extensions;
 using Zarf.Query.Expressions;
+using System.Reflection;
 
 namespace Zarf.Query.ExpressionVisitors
 {
-    public class Projection
-    {
-        public Expression Expression { get; set; }
-
-        public int Ordinal { get; set; }
-    }
-
     public class ProjectionExpressionVisitor : ExpressionVisitor, IProjectionScanner
     {
-        private List<Projection> _projections;
-
-        protected override Expression VisitExtension(Expression node)
-        {
-            if (node.Is<ColumnExpression>() || node.Is<AggregateExpression>())
-            {
-                _projections.Add(new Projection()
-                {
-                    Expression = node,
-                    Ordinal = _projections.Count
-                });
-            }
-            else if (node.Is<FromTableExpression>())
-            {
-                foreach (var item in node.As<FromTableExpression>().GenerateColumns())
-                {
-                    _projections.Add(new Projection()
-                    {
-                        Expression = item,
-                        Ordinal = _projections.Count
-                    });
-                }
-            }
-
-            return node;
-        }
+        private List<Projection> _list;
 
         public List<Projection> Scan(Expression node)
         {
-            _projections = new List<Projection>();
+            _list = new List<Projection>();
             Visit(node);
-            return _projections.ToList();
+            return _list.ToList();
         }
 
         public List<Projection> Scan(Func<Expression, Expression> preHandle, Expression node)
         {
             return Scan(preHandle(node));
         }
+
+        protected override Expression VisitExtension(Expression node)
+        {
+            return node;
+        }
+
+        protected override Expression VisitMemberInit(MemberInitExpression memberInit)
+        {
+            Visit(memberInit.NewExpression);
+
+            foreach (var binding in memberInit.Bindings.OfType<MemberAssignment>())
+            {
+                AddProjection(binding.Member, binding.Expression);
+            }
+
+            return memberInit;
+        }
+
+        protected override Expression VisitNew(NewExpression newExp)
+        {
+            if (newExp.Arguments == null || newExp.Arguments.Count == 0)
+            {
+                return newExp;
+            }
+
+            for (var i = 0; i < newExp.Arguments.Count; i++)
+            {
+                AddProjection(newExp.Members[i], newExp.Arguments[i]);
+            }
+
+            return newExp;
+        }
+
+        private void AddProjection(MemberInfo member, Expression node)
+        {
+            if (node.NodeType != ExpressionType.Extension)
+            {
+                Visit(node);
+                return;
+            }
+
+            if (node.Is<FromTableExpression>())
+            {
+                foreach (var item in node
+                    .As<FromTableExpression>()
+                    .GenerateColumns()
+                    .OfType<ColumnExpression>())
+                {
+                    AddProjection(item.Member, item);
+                }
+
+                return;
+            }
+
+            if (node.Is<ColumnExpression>() ||
+                node.Is<AggregateExpression>())
+            {
+                _list.Add(new Projection() { Member = member, Expression = node, Ordinal = _list.Count });
+            }
+        }
+
+
     }
 }
